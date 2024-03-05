cd1d1ca07d2ac53a0781e2dc3a038de35c6c0b3d
SpanDuy <hdhrbux@gmail.com>
2024-03-05 17:57:55 +0300

    init: init project

---
+++ src/main/java/com/example/api/CandidateManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandidateManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandidateManagementServiceApplication.class, args);
+	}
+
+}
